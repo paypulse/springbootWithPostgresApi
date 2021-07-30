92cd57afa5ac229f6a4d71ef2411a0736211f545
paypulse <sourcedev5@gmail.com>
2021-07-30 18:59:24 +0900

    배포

--- src/main/java/com/example/crmtestexample/CrmTestExampleApplication.java
+++ src/main/java/com/example/crmtestexample/CrmTestExampleApplication.java
@@ -2,11 +2,21 @@ package com.example.crmtestexample;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class CrmTestExampleApplication {
+public class CrmTestExampleApplication extends SpringBootServletInitializer {
+
+
+    /*war 배포를 위한 소스 */
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(CrmTestExampleApplication.class);
+    }
 
     public static void main(String[] args) {
+
         SpringApplication.run(CrmTestExampleApplication.class, args);
     }
 
